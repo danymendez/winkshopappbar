d1e9010f68bce3776e5800e27ede4c103506c5d5
Daniel Méndez Palacios <dmendezp@outlook.com>
2018-04-26 17:01:07 -0600

    Se suben vistas del proyecto

--- app/src/main/java/com/example/palacios/winkshopappbar/RegistrarActivity.java
+++ app/src/main/java/com/example/palacios/winkshopappbar/RegistrarActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class RegistrarActivity extends AppCompatActivity {
 
+    //Hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
