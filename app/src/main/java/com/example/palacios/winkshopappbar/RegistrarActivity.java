5ee065e246895d5058d78ce0ada6f7b66b1e89b6
danymendez <daniel.mendez.uml@gmail.com>
2018-05-13 10:49:48 -0600

    se agrega inputextlayout para mejor visualización

--- app/src/main/java/com/example/palacios/winkshopappbar/RegistrarActivity.java
+++ app/src/main/java/com/example/palacios/winkshopappbar/RegistrarActivity.java
@@ -1,6 +1,7 @@
 package com.example.palacios.winkshopappbar;
 
 import android.app.ProgressDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -31,27 +32,27 @@ public class RegistrarActivity extends AppCompatActivity {
 
 
     Button registrarBtn;
-    EditText edNombre,edApellido,edFechaNac,edDireccion,edTelefono,edCelular,edUsuario,edPassword;
+    TextInputLayout edNombre,edApellido,edFechaNac,edDireccion,edTelefono,edCelular,edUsuario,edPassword;
     Spinner spinnerPais,spinnerDepartamento;
     WinkShopHelpers winkShopHelpers = new WinkShopHelpers();
     WinkShopService service;
     Usuarios usuarios;
     Clientes clientes;
-    List<Paises> listaPaises;
+    LinkedList<Paises> listaPaises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
 
-        edNombre = getEditText(R.id.edNombre);
-        edApellido = getEditText(R.id.edApellidos);
-        edFechaNac = getEditText(R.id.edFechaNac);
-        edDireccion = getEditText(R.id.edDireccion);
-        edTelefono = getEditText(R.id.edTelefono);
-        edCelular = getEditText(R.id.edCelular);
-        edUsuario = getEditText(R.id.edUsuario);
-        edPassword = getEditText(R.id.edPassword);
+        edNombre = getEditTextFromTextInputLayout(R.id.tlNombres);
+        edApellido = getEditTextFromTextInputLayout(R.id.tlApellidos);
+        edFechaNac = getEditTextFromTextInputLayout(R.id.tlFechaNac);
+        edDireccion = getEditTextFromTextInputLayout(R.id.tlDireccion);
+        edTelefono = getEditTextFromTextInputLayout(R.id.tlTelefono);
+        edCelular = getEditTextFromTextInputLayout(R.id.tlCelular);
+        edUsuario = getEditTextFromTextInputLayout(R.id.tlUsuario);
+        edPassword = getEditTextFromTextInputLayout(R.id.tlPassword);
         registrarBtn = getButton(R.id.RegistrarBtn);
         spinnerPais = getSpinner(R.id.spinnerPais);
 
@@ -66,7 +67,7 @@ public class RegistrarActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Paises>> call, Response<List<Paises>> response) {
               if(response.isSuccessful()){
-                  listaPaises=response.body();
+
                     ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,response.body());
                     spinnerPais.setAdapter(arrayAdapter);
                   progressDialog.dismiss();
@@ -90,7 +91,7 @@ public class RegistrarActivity extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if(!hasFocus){
-                    service.getUsuario(edUsuario.getText().toString().trim()).enqueue(new Callback<List<Usuarios>>() {
+                    service.getUsuario(edUsuario.getEditText().getText().toString().trim()).enqueue(new Callback<List<Usuarios>>() {
                         @Override
                         public void onResponse(Call<List<Usuarios>> call, Response<List<Usuarios>> response) {
                             if(response.isSuccessful()){
@@ -139,33 +140,27 @@ public class RegistrarActivity extends AppCompatActivity {
 
                     //Llenar información de usuario en la clase
                     usuarios.setIdUsuario(0);
-                    usuarios.setUsuario(edUsuario.getText().toString().trim());
-                    usuarios.setPassword(edPassword.getText().toString().trim());
-                    usuarios.setNombres(edNombre.getText().toString().trim());
-                    usuarios.setApellidos(edApellido.getText().toString().trim());
+                    usuarios.setUsuario(edUsuario.getEditText().getText().toString().trim());
+                    usuarios.setPassword(edPassword.getEditText().getText().toString().trim());
+                    usuarios.setNombres(edNombre.getEditText().getText().toString().trim());
+                    usuarios.setApellidos(edApellido.getEditText().getText().toString().trim());
                     usuarios.setCreatedOn(new Date());
                     usuarios.setInactivo(false);
                     usuarios.setIdRol(2);
 
-                    //Llenar Información de cliente acorde a los controles 
+                    //Llenar Información de cliente
                     clientes.setIdCliente(0);
                     clientes.setNombre(usuarios.getNombres());
                     clientes.setApellido(usuarios.getApellidos());
-                    clientes.setTelefono(edTelefono.getText().toString().trim());
-                    clientes.setCelular(edCelular.getText().toString().trim());
-                   for(Paises i : listaPaises){
-                       if(i.NombrePais.trim().equals(spinnerPais.getSelectedItem().toString().trim())){
-                           clientes.setIdPais(i.IdPais);
-                       }
-                   }
-
+                    clientes.setTelefono(edTelefono.getEditText().getText().toString().trim());
+                    clientes.setCelular(edCelular.getEditText().getText().toString().trim());
                     clientes.setRazonSocial("None");
-                    clientes.setDireccion(edDireccion.getText().toString().trim());
+                    clientes.setDireccion(edDireccion.getEditText().getText().toString().trim());
                     clientes.setTipoCliente("N");
                     clientes.setCreatedOn(new Date());
                     clientes.setInactivo(false);
 
-                    service.getUsuario(edUsuario.getText().toString().trim()).enqueue(new Callback<List<Usuarios>>() {
+                    service.getUsuario(edUsuario.getEditText().getText().toString().trim()).enqueue(new Callback<List<Usuarios>>() {
                         @Override
                         public void onResponse(Call<List<Usuarios>> call, Response<List<Usuarios>> response) {
                             if(response.isSuccessful()){
@@ -231,6 +226,10 @@ public class RegistrarActivity extends AppCompatActivity {
         return (EditText)findViewById(id);
     }
 
+    public TextInputLayout getEditTextFromTextInputLayout(int id){
+        return (TextInputLayout)findViewById(id);
+    }
+
     public TextView getTextView(int id){
         return (TextView)findViewById(id);
     }
@@ -239,9 +238,9 @@ public class RegistrarActivity extends AppCompatActivity {
         return (Spinner) findViewById(id);
     }
 
-    public boolean validarEditText(EditText editText,String Mensaje){
+    public boolean validarEditText(TextInputLayout editText,String Mensaje){
         boolean estaVacio = false;
-        if(TextUtils.isEmpty(editText.getText().toString().trim())){
+        if(TextUtils.isEmpty(editText.getEditText().getText().toString().trim())){
             editText.setError(Mensaje);
             editText.requestFocus();
             estaVacio = true;
