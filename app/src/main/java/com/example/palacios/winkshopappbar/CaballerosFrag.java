6a63cb467ccc8bbd475d267a4d716cd85af09ed2
Daniel Méndez Palacios <dmendezp@outlook.com>
2018-05-15 11:56:15 -0600

    Se agrega fragment caballero

--- app/src/main/java/com/example/palacios/winkshopappbar/CaballerosFrag.java
+++ app/src/main/java/com/example/palacios/winkshopappbar/CaballerosFrag.java
@@ -1,5 +1,6 @@
 package com.example.palacios.winkshopappbar;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,6 +8,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import Adapters.AdaptadorOfertas;
+import Models.Ofertas;
+import Models.Productos;
+import Services.WinkShopHelpers;
+import Services.WinkShopService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -19,6 +33,13 @@ public class CaballerosFrag extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    ListView listView;
+    List<Productos> listaProductos;
+    WinkShopHelpers winkShopHelpers = new WinkShopHelpers();
+    String[] productos,descripcion;
+    double[] precio;
+    int[] imagenes;
+
     public CaballerosFrag() {
         // Required empty public constructor
     }
@@ -28,6 +49,58 @@ public class CaballerosFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_caballeros, container, false);
+        listView = view.findViewById(R.id.ListViewPpalCaballero);
+
+
+        final WinkShopService service = winkShopHelpers.retrofit.create(WinkShopService.class);
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity().getApplicationContext());
+
+
+        progressDialog.setMax(100);
+        progressDialog.setTitle("");
+        progressDialog.setMessage("Cargando");
+        progressDialog.show();
+
+        service.getProductos().enqueue(new Callback<List<Productos>>() {
+            @Override
+            public void onResponse(Call<List<Productos>> call, Response<List<Productos>> response) {
+                if(response.isSuccessful()){
+
+                    listaProductos = response.body();
+
+
+                    int tamanio = listaProductos.size();
+                    productos = new String[tamanio];
+                    descripcion = new String[tamanio];
+                    precio = new double[tamanio];
+                    imagenes = new int[tamanio];
+
+                    for(int i =0;i<listaProductos.size();i++){
+                        productos[i] = listaProductos.get(i).getNombreProducto();
+                        descripcion[i] = listaProductos.get(i).getDescripcion();
+                        precio[i] = listaProductos.get(i).getPrecio();
+                        imagenes[i] = R.drawable.camisaverde;
+                    }
+
+                    progressDialog.dismiss();
+
+                    final  AdaptadorOfertas adaptadorOfertas = new AdaptadorOfertas(getActivity().getApplicationContext(),productos,imagenes,descripcion,precio);
+                    listView.setAdapter(adaptadorOfertas);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Productos>> call, Throwable t) {
+
+            }
+        });
+
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_caballeros, container, false);
     }
 
