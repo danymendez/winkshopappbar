37b55f7223cdb790f188cfe850b81eb365269ea4
Daniel Méndez Palacios <dmendezp@outlook.com>
2018-05-24 08:06:51 -0600

    modificaciones en layout

--- app/src/main/java/Adapters/BaseAdaptador.java
+++ app/src/main/java/Adapters/BaseAdaptador.java
@@ -1,6 +1,7 @@
 package Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.palacios.winkshopappbar.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Models.Productos;
 
 public class BaseAdaptador extends BaseAdapter {
 
@@ -20,6 +26,10 @@ public class BaseAdaptador extends BaseAdapter {
     private double[] precio;
     private View item;
 
+    List<Productos> listaproductos;
+    private ArrayList<Integer> cantidades;
+    private Bitmap[] bitmaps;
+
     public BaseAdaptador(Context context, String[] productos, int[] imagenes, String[] descripcion,double[] precio) {
 
         this.productos = productos;
@@ -28,6 +38,15 @@ public class BaseAdaptador extends BaseAdapter {
         this.descripcion = descripcion;
         this.precio = precio;
     }
+
+    public BaseAdaptador(Context context,List<Productos> listaproductos,  ArrayList<Integer> cantidades,Bitmap[] bitmaps) {
+
+   this.listaproductos = listaproductos;
+   this.cantidades = cantidades;
+   this.bitmaps = bitmaps;
+    }
+
+
 
     @Override
     public int getCount() {
@@ -48,16 +67,16 @@ public class BaseAdaptador extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
 
         if(view==null){
-            view = LayoutInflater.from(context).inflate(R.layout.listofertaslayout,viewGroup,false);
+            view = LayoutInflater.from(context).inflate(R.layout.listacarritolayout,viewGroup,false);
         }
-        TextView tvProducto =(TextView)view.findViewById(R.id.tvProducto);
-        tvProducto.setText(productos[i]);
-        TextView tvDescripcion =(TextView)view.findViewById(R.id.tvDescripcion);
-        tvDescripcion.setText(descripcion[i]);
-        TextView tvPrecio = (TextView)view.findViewById(R.id.tvPrecio);
-        tvPrecio.setText("$"+precio[i]);
-        ImageView imageView = (ImageView) view.findViewById(R.id.imageViewProducto);
-        imageView.setImageResource(imagenes[i]);
+        TextView tvProducto =(TextView)view.findViewById(R.id.tvProductoCarritoDet);
+        tvProducto.setText(listaproductos.get(i).getNombreProducto());
+        TextView tvDescripcion =(TextView)view.findViewById(R.id.tvDescripcionCarritoDet);
+        tvDescripcion.setText(listaproductos.get(i).getDescripcion());
+        TextView tvPrecio = (TextView)view.findViewById(R.id.tvPrecioCarritoDet);
+        tvPrecio.setText("$"+(listaproductos.get(i).getPrecio()*cantidades.get(i)));
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageViewProductoCarritoDet);
+        imageView.setImageBitmap(bitmaps[i]);
         return view;
     }
 }
--- app/src/main/java/com/example/palacios/winkshopappbar/ListaCarritoActivity.java
+++ app/src/main/java/com/example/palacios/winkshopappbar/ListaCarritoActivity.java
@@ -1,13 +1,86 @@
 package com.example.palacios.winkshopappbar;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import Adapters.BaseAdaptador;
+import Models.ListasProductosSing;
+import Models.Ofertas;
+import Models.Productos;
 
 public class ListaCarritoActivity extends AppCompatActivity {
-
+    ListView listView;
+    List<Productos> listaproductosfiltrado;
+    List<Productos> listaproductos;
+    List<Ofertas> listaOfertas;
+    ArrayList<Integer> IdsAComprar;
+    ArrayList<Integer> IdsAComprarFiltradas;
+    ArrayList<Integer>  cantidades;
+    ArrayList<Integer> cantidadesFiltradas;
+    Bitmap[] bitmaps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_carrito);
+
+        listView = findViewById(R.id.listViewCarritoDet);
+
+      listaproductos = new LinkedList<Productos>();
+      listaproductos = ListasProductosSing.getListaProductos();
+
+      listaOfertas = new LinkedList<Ofertas>();
+      listaOfertas = ListasProductosSing.getListaOfertas();
+
+      IdsAComprar = ListasProductosSing.arrayListIdProductos;
+
+
+      for(int i = 0;i<IdsAComprar.size();i++){
+
+          IdsAComprarFiltradas.add(IdsAComprar.get(i));
+
+          if(IdsAComprarFiltradas.size()>0) {
+              for (int o = 0; o < IdsAComprarFiltradas.size(); o++) {
+                  if (IdsAComprar.get(i) == IdsAComprarFiltradas.get(o)) {
+                        cantidades.set(o,cantidades.get(o)+1);
+                  }else{
+                      IdsAComprarFiltradas.add(IdsAComprar.get(i));
+                  }
+              }
+          }else{
+              IdsAComprarFiltradas.add(IdsAComprar.get(i));
+              cantidades.add(1);
+          }
+      }
+
+      //Identificando cuales son los productos a comprar;
+      for(int i =0;i<IdsAComprarFiltradas.size();i++){
+          if(listaproductos.get(i).getIdProducto()==IdsAComprarFiltradas.get(i)){
+              listaproductosfiltrado.add(listaproductos.get(i));
+          }
+      }
+
+      bitmaps = new Bitmap[listaproductosfiltrado.size()];
+
+        for(int i =0; i<listaproductos.size();i++){
+
+            for(int o =0;o<listaproductosfiltrado.size();o++){
+                if(listaproductosfiltrado.get(i).getIdProducto()==listaproductos.get(i).getIdProducto()){
+                    bitmaps[o] = ListasProductosSing.getBitmaps()[i];
+                }
+            }
+        }
+
+        listView.setAdapter(new BaseAdaptador(getApplicationContext(),listaproductos,cantidades,bitmaps));
+
+
     }
 }
